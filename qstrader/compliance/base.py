bd9f059f0c13598188b03653542791a7ee27e6ad
fzanuso <franco.zanuso@quantex.com.ar>
2024-04-20 20:29:46 +0100

    update initial commit

--- qstrader/compliance/base.py
+++ qstrader/compliance/base.py
@@ -4,7 +4,7 @@ from abc import ABCMeta, abstractmethod
 class AbstractCompliance(object):
     """
     The Compliance component should be given every trade
-    that occurs in qstrader.
+    that occurs in qstrader_aux.
 
     It is designed to keep track of anything that may
     be required for regulatory or audit (or debugging)
